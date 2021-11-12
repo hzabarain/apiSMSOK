f07c4e85904eacaf59b1478ee1735e461de48a1d
Your Name <you@example.com>
2021-11-12 06:24:13 -0500

    v1

--- index.py
+++ index.py
@@ -39,6 +39,29 @@ def send_sms():
         return "error"
 
  ####hasta aca nueco
+
+ @app.route('/send_sms', methods=['POST'])
+def send_sms():
+    try:
+        account_sid = env['TWILIO_ACCOUNT_SID']
+        auth_token = env['TWILIO_AUTH_TOKEN']
+        origen = env['TWILIO_PHONE_NUMBER']
+        client = Client(account_sid, auth_token)
+        data = request.json
+        contenido = data["contenido"]
+        destino = data["destino"]
+       
+        message = client.messages.create(
+                            body=contenido,
+                            from_=origen,
+                            to='+57' + destino
+                        )
+        print(message)
+        return "send success"
+    except Exception as e:
+        print(e)
+        return "error"
+
  #Ejecutamos el servidor
 if __name__ == '__main__':
     app.run()
